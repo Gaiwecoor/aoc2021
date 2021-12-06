2cac02c1c2d46ac85454ba3c13aa64af98584552
Gaiwecoor <gaiwecoor@gmail.com>
2021-12-05 21:14:50 -0600

    Additional Classes

--- tools.js
+++ tools.js
@@ -57,6 +57,134 @@ class Link {
   }
 }
 
+class Point {
+  constructor(data) {
+    if (typeof data == "string") {
+      const [x, y] = data.split(",").map(n => parseInt(n, 10));
+      this.x = x;
+      this.y = y;
+    } else {
+      this.x = data.x;
+      this.y = data.y;
+    }
+  }
+
+  distanceTo(point) {
+    return Math.abs(this.x - point.x) + Math.abs(this.y - point.y);
+  }
+
+  get label() {
+    return `${this.x},${this.y}`;
+  }
+}
+
+class UMap extends Map {
+  constructor(data) {
+    super(data);
+  }
+
+  filter(fn) {
+    const filtered = new this.constructor();
+    for (const [key, value] of this) {
+      if (fn(value, key, this)) filtered.set(key, value);
+    }
+    return filtered;
+  }
+
+  find(fn) {
+    for (const [key, value] of this) {
+      if (fn(value, key, this)) return value;
+    }
+    return undefined;
+  }
+
+  first() {
+    for (const [key, value] of this) {
+      return value;
+    }
+  }
+
+  last() {
+    let val;
+    for (const [key, value] of this) {
+      val = value;
+    }
+    return val;
+  }
+
+  map(fn) {
+    const mapped = Array(this.size);
+    let i = 0;
+
+    for (const [key, value] of this) {
+      mapped[i++] = fn(value, key, this);
+    }
+
+    return mapped;
+  }
+
+  some(fn) {
+    for (const [key, value] of this) {
+      if (fn(value, key, this)) return true;
+    }
+    return false;
+  }
+}
+
+class USet extends Set {
+  constructor(data) {
+    super(data);
+  }
+
+  filter(fn) {
+    const filtered = new this.constructor();
+    for (const value of this) {
+      if (fn(value, this)) filtered.add(value);
+    }
+    return filtered;
+  }
+
+  find(fn) {
+    for (const value of this) {
+      if (fn(value, this)) return value;
+    }
+    return undefined;
+  }
+
+  first() {
+    for (const value of this) {
+      return value;
+    }
+  }
+
+  last() {
+    let val;
+    for (const value of this) {
+      val = value;
+    }
+    return val;
+  }
+
+  map(fn) {
+    const mapped = Array(this.size);
+    let i = 0;
+    for (const value of this) {
+      mapped[i++] = fn(value, this);
+    }
+    return mapped;
+  }
+
+  some(fn) {
+    for (const value of this) {
+      if (fn(value, this)) return true;
+    }
+    return false;
+  }
+}
+
 module.exports = {
-  Link
+  Link,
+  Point,
+  UMap,
+  USet,
 };
